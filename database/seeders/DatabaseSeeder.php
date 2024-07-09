a05ebba4308a2d1a8bfcb6279a066e0d44e158e9
Kirami Fahd <kiramifahd@gmail.com>
2024-07-09 14:37:59 +0100

    seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call(CategorySeeder::class);
+       // $this->call(CategorySeeder::class);
         $this->call(UserSeeder::class);
         // User::factory(10)->create();
 
